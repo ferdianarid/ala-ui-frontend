1ee77b8d174ab098135c9bcdc01a52d3e8870d76
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-09 23:41:59 +0700

    update navbar and components

--- components/Facility.tsx
+++ components/Facility.tsx
@@ -43,12 +43,12 @@ export default function Facility() {
                                           <Popover.Button
                                                  className={`
 								${open ? '' : 'text-opacity-90'}
-								text-blue-600 group inline-flex items-center text-base font-medium hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+								text-gray-900  group inline-flex items-center text-base font-medium hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
                                           >
                                                  <span className="text-sm">Facility</span>
                                                  <ChevronDownIcon
                                                         className={`${open ? '' : 'text-opacity-70'}
-									ml-2 h-5 w-5 text-orange-300 group-hover:text-opacity-80 transition ease-in-out duration-150`}
+									ml-2 h-5 w-5 text-gray-900 group-hover:text-opacity-80 transition ease-in-out duration-150`}
                                                         aria-hidden="true"
                                                  />
                                           </Popover.Button>
@@ -119,24 +119,24 @@ function IconOne() {
                      fill="none"
                      xmlns="http://www.w3.org/2000/svg"
               >
-                     <rect width="48" height="48" rx="8" fill="#edeeff" />
+                     <rect width="48" height="48" rx="8" fill="#FFEDD5" />
                      <path
                             d="M24 11L35.2583 17.5V30.5L24 37L12.7417 30.5V17.5L24 11Z"
-                            stroke="#5762ff"
+                            stroke="#FB923C"
                             strokeWidth="2"
                      />
                      <path
                             fillRule="evenodd"
                             clipRule="evenodd"
                             d="M16.7417 19.8094V28.1906L24 32.3812L31.2584 28.1906V19.8094L24 15.6188L16.7417 19.8094Z"
-                            stroke="#9199ff"
+                            stroke="#FDBA74"
                             strokeWidth="2"
                      />
                      <path
                             fillRule="evenodd"
                             clipRule="evenodd"
                             d="M20.7417 22.1196V25.882L24 27.7632L27.2584 25.882V22.1196L24 20.2384L20.7417 22.1196Z"
-                            stroke="#9199ff"
+                            stroke="#FDBA74"
                             strokeWidth="2"
                      />
               </svg>
@@ -152,17 +152,17 @@ function IconTwo() {
                      fill="none"
                      xmlns="http://www.w3.org/2000/svg"
               >
-                     <rect width="48" height="48" rx="8" fill="#edeeff" />
+                     <rect width="48" height="48" rx="8" fill="#FFEDD5" />
                      <path
                             d="M28.0413 20L23.9998 13L19.9585 20M32.0828 27.0001L36.1242 34H28.0415M19.9585 34H11.8755L15.9171 27"
-                            stroke="#5762ff"
+                            stroke="#FB923C"
                             strokeWidth="2"
                      />
                      <path
                             fillRule="evenodd"
                             clipRule="evenodd"
                             d="M18.804 30H29.1963L24.0001 21L18.804 30Z"
-                            stroke="#9199ff"
+                            stroke="#FDBA74"
                             strokeWidth="2"
                      />
               </svg>
@@ -178,13 +178,13 @@ function IconThree() {
                      fill="none"
                      xmlns="http://www.w3.org/2000/svg"
               >
-                     <rect width="48" height="48" rx="8" fill="#edeeff" />
-                     <rect x="13" y="32" width="2" height="4" fill="#9199ff" />
-                     <rect x="17" y="28" width="2" height="8" fill="#9199ff" />
-                     <rect x="21" y="24" width="2" height="12" fill="#9199ff" />
-                     <rect x="25" y="20" width="2" height="16" fill="#9199ff" />
-                     <rect x="29" y="16" width="2" height="20" fill="#5762ff" />
-                     <rect x="33" y="12" width="2" height="24" fill="#5762ff" />
+                     <rect width="48" height="48" rx="8" fill="#FFEDD5" />
+                     <rect x="13" y="32" width="2" height="4" fill="#FDBA74" />
+                     <rect x="17" y="28" width="2" height="8" fill="#FDBA74" />
+                     <rect x="21" y="24" width="2" height="12" fill="#FDBA74" />
+                     <rect x="25" y="20" width="2" height="16" fill="#FDBA74" />
+                     <rect x="29" y="16" width="2" height="20" fill="#FB923C" />
+                     <rect x="33" y="12" width="2" height="24" fill="#FB923C" />
               </svg>
        )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -29,34 +29,34 @@ function Navbar() {
                                    <div className="hidden md:block">
                                           <div className="ml-10 flex items-center space-x-4">
                                                  <Link href="/">
-                                                        <a className=" text-blue-600 hover:bg-blue-800 hover:text-white px-3 py-2 rounded-xl text-sm font-medium">
+                                                        <a className=" text-gray-900 hover:bg-yellow-500 hover:text-white px-3 py-2 rounded-xl text-sm font-medium">
                                                                Homepage
                                                         </a>
                                                  </Link>
                                                  <Link href="/tickets">
-                                                        <a className="text-blue-600 hover:bg-blue-800 hover:text-white px-3 py-2 rounded-xl text-sm font-medium">
+                                                        <a className="text-gray-900 hover:bg-yellow-500 hover:text-white px-3 py-2 rounded-xl text-sm font-medium">
                                                                Tickets
                                                         </a>
                                                  </Link>
                                                  {/* facility dropdown */}
                                                  <Facility />
                                                  <Link href="/category">
-                                                        <a className="text-blue-600 hover:bg-blue-800 hover:text-white px-3 py-2 rounded-xl text-sm font-medium">
+                                                        <a className="text-gray-900 hover:bg-yellow-500 hover:text-white px-3 py-2 rounded-xl text-sm font-medium">
                                                                Category
                                                         </a>
                                                  </Link>
                                                  <Link href="/contact">
-                                                        <a className="text-blue-600 hover:bg-blue-800 hover:text-white px-3 py-2 rounded-xl text-sm font-medium">
+                                                        <a className="text-gray-900 hover:bg-yellow-500 hover:text-white px-3 py-2 rounded-xl text-sm font-medium">
                                                                Contact
                                                         </a>
                                                  </Link>
                                                  <Link href="/cart">
-                                                        <div className="flex justify-between items-center bg-blue-50 rounded-md py-2 px-4">
-                                                               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-600" viewBox="0 0 20 20" fill="currentColor">
+                                                        <div className="flex justify-between items-center bg-yellow-100 rounded-md py-2 px-4">
+                                                               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-yellow-500" viewBox="0 0 20 20" fill="currentColor">
                                                                       <path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" />
                                                                </svg>
                                                                <div className="count">
-                                                                      <h1 className="text-md font-bold text-blue-600 ml-2">100</h1>
+                                                                      <h1 className="text-md font-bold text-yellow-500 ml-2">100</h1>
                                                                </div>
                                                         </div>
                                                  </Link>
@@ -67,7 +67,7 @@ function Navbar() {
                                    <button
                                           onClick={() => setIsOpen(!isOpen)}
                                           type="button"
-                                          className="bg-white inline-flex items-center justify-center p-2 rounded-md text-blue-800 hover:text-white hover:bg-blue-800 focus:outline-none "
+                                          className="bg-white inline-flex items-center justify-center p-2 rounded-md text-gray-900 hover:text-white hover:bg-blue-800 focus:outline-none "
                                           aria-controls="mobile-menu"
                                           aria-expanded="false"
                                    >
@@ -123,13 +123,13 @@ function Navbar() {
                                           <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                                                  <Link
                                                         href="/">
-                                                        <a className="hover:bg-blue-800 text-sm text-blue-600 hover:text-white block px-3 py-2 rounded-md font-medium">
+                                                        <a className="hover:bg-yellow-500 text-sm text-gray-900 hover:text-white block px-3 py-2 rounded-md font-medium">
                                                                Homepage
                                                         </a>
                                                  </Link>
 
                                                  <Link href="/tickets">
-                                                        <a className="text-blue-600 text-sm hover:bg-blue-800 hover:text-white block px-3 py-2 rounded-md font-medium">
+                                                        <a className="text-gray-900 text-sm hover:bg-yellow-500 hover:text-white block px-3 py-2 rounded-md font-medium">
                                                                Tickets
                                                         </a>
                                                  </Link>
@@ -138,24 +138,24 @@ function Navbar() {
 
                                                  <Link
                                                         href="/category">
-                                                        <a className="text-blue-600 text-sm hover:bg-blue-800 hover:text-white block px-3 py-2 rounded-md font-medium">
+                                                        <a className="text-gray-900 text-sm hover:bg-yellow-500 hover:text-white block px-3 py-2 rounded-md font-medium">
                                                                Category
                                                         </a>
                                                  </Link>
 
                                                  <Link
                                                         href="/contact">
-                                                        <a className="text-blue-600 text-sm hover:bg-blue-800 hover:text-white block px-3 py-2 rounded-md font-medium">
+                                                        <a className="text-gray-900 text-sm hover:bg-yellow-500 hover:text-white block px-3 py-2 rounded-md font-medium">
                                                                Contact
                                                         </a>
                                                  </Link>
                                                  <Link href="/cart">
-                                                        <div className="flex justify-start md:w-auto w-24 md:justify-between items-center bg-blue-50 rounded-md py-2 px-4">
-                                                               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-600" viewBox="0 0 20 20" fill="currentColor">
+                                                        <div className="flex justify-start md:w-auto w-24 md:justify-between items-center bg-yellow-100 rounded-md py-2 px-4">
+                                                               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-yellow-500" viewBox="0 0 20 20" fill="currentColor">
                                                                       <path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" />
                                                                </svg>
                                                                <div className="count">
-                                                                      <h1 className="text-md font-bold text-blue-600 ml-2">100</h1>
+                                                                      <h1 className="text-md font-bold text-yellow-500 ml-2">100</h1>
                                                                </div>
                                                         </div>
                                                  </Link>
