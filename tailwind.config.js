12b04c6c09297957e7df439c8a09dcd2f97f57be
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-22 09:06:27 +0700

    add layout path to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     mode: "jit",
-    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", ],
+    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./layouts/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {},
     },
