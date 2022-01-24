ba5e52fb2cb4a78c7104a7174275dc02d5a13d2b
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-24 13:31:43 +0700

    update badges

--- components/atoms/badges/index.tsx
+++ components/atoms/badges/index.tsx
@@ -4,6 +4,10 @@ interface IBadges {
        children: ReactNode
 }
 
+interface ICategoryBadge {
+       children: ReactNode
+}
+
 export const PrimaryBadges = ({ children }: IBadges) => {
        return (
               <div className="py-1 px-3 bg-blue-200 border border-blue-600 rounded-full text-blue-500 bg-opacity-25 w-fit font-bold text-xs mb-4 md:mb-2">{children}</div>
@@ -25,5 +29,12 @@ export const WarningBadges = ({ children }: IBadges) => {
 export const DangerBadges = ({ children }: IBadges) => {
        return (
               <div className="py-1 px-3 bg-red-200 border border-red-600 rounded-full text-red-500 bg-opacity-25 w-fit font-bold text-xs mb-4 md:mb-2">{children}</div>
+       )
+}
+
+export const SecondaryCategoryBadge = (props: ICategoryBadge) => {
+       const { children } = props
+       return (
+              <div className="absolute z-10 left-6 top-6 py-[2px] px-2 bg-yellow-500 bg-opacity-70 text-white text-xs border-2 border-yellow-500 font-bold rounded-full">{children}</div>
        )
 }
